4b06e5afcf2190175e362cb6723b776feeb7445a
Lucaseg685 <142328194+Lucaseg685@users.noreply.github.com>
2023-08-22 09:09:37 -0500

    Add files via upload

---
+++ Primeros Programas/fifthProgram.py.py
@@ -0,0 +1,20 @@
+coffee_price = 1.50
+number_of_coffees = 4
+
+# Prints "6.0"
+print("Coffe*number=1.50*4 =",coffee_price * number_of_coffees)
+# Prints "1.5"
+print("Coffe=",coffee_price)
+# Prints "4"
+print("number =",number_of_coffees)
+
+# Updating the price
+coffee_price = 2.00
+print("Cambian los valores")
+
+# Prints "2.0"
+print("Coffe*number=2*4 =", coffee_price * number_of_coffees)
+# Prints "8.0"
+print("Coffe =", coffee_price)
+# Pints "4"
+print("number =", number_of_coffees)
---
+++ Primeros Programas/fourthProgram.py
@@ -0,0 +1,4 @@
+print("la operacion 25*68+13/28 es Igual a")
+print(25 * 68 + 13 / 28)
+print("la operacion (25*68) + (13/28) es Igual a")
+print((25 * 68) + (13 / 28))
